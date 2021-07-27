0158e65c9dd40b10d7cf27d201345bd30778251c
Stanislav Bober <sbober@miem.hse.ru>
2021-07-27 21:56:23 +0300

    upd: ev_generator.py - updated code extraction logic

--- fastrk/ev_generator.py
+++ fastrk/ev_generator.py
@@ -3,8 +3,9 @@ import importlib
 import inspect
 import os
 import textwrap
+import re
 
-default_jitkwargs = {'nopython': True, 'nogil': True, 'fastmath': True, 'cache': True}
+default_jitkwargs = {'nopython': True, 'nogil': True, 'fastmath': True, 'cache': True, 'boundscheck': False}
 
 
 def _py_func(f):
@@ -40,99 +41,132 @@ def generate_call_event(n, **jitkwargs):
     return '\n'.join(lst)
 
 
-def extract_function_code(fun):
-    try:
-        text = inspect.getsource(_py_func(fun))
-        return text
-    except:
-        return ''
+def unpack_cvars(obj, attr=None, code='', exclude_vars=()):
+    vars = {}
+    funcs = {}
+    modules = {}
 
-
-def extract_method_code(obj, method):
-    try:
-        text = inspect.getsource(getattr(obj, method))
-        return text
-    except:
-        return ''
-
-
-def extract_closure_vars(obj):
-    try:
+    if attr is None:
         cvars = inspect.getclosurevars(obj)
-        return cvars
-    except:
-        return None
+    else:
+        cvars = inspect.getclosurevars(attr)
 
-
-def extract_code(obj, exclude_vars=tuple(), method='__call__'):
-    """
-    Here obj can be:
-    - object which method's code should be extracted with dependencies
-    - function which code should be extracted with dependencies
-    - builtin which code can't be extracted therefore import this builtin
-    """
-    obj_ = _py_func(obj)
-
-    if inspect.isbuiltin(obj_):
-        return f'from {obj_.__module__} import {obj_.__name__}'
-    elif inspect.isfunction(obj_):
-        code = '\n' + extract_function_code(obj_).split(':\n')[1]
-        deps = extract_deps_code(obj_)
-        return deps, code
-
-    code = extract_method_code(obj_, method)
-    cvars = extract_closure_vars(getattr(obj_, method))
-
-    deps = ''
-    text = ''
-    for name, g in cvars.globals.items():
-        if hasattr(g, '__call__') or inspect.ismodule(obj_):
-            deps += extract_deps_code(g, name=name)
-        else:
-            text += f'    {name} = {repr(g)}\n'
+    for d in (cvars.nonlocals.items(), cvars.globals.items(), cvars.builtins.items()):
+        for name, value in d:
+            if name in exclude_vars:
+                continue
+            value = _py_func(value)
+            if inspect.ismodule(value):
+                modules[name] = value
+            elif inspect.isbuiltin(value) or inspect.isfunction(value):
+                funcs[name] = value
+                r = re.search(rf'(\w+).{name}', code)
+                if r is not None and r.group() != 'self':
+                    funcs[f'{r.group()}.{name}'] = value
+            elif inspect.isclass(value):
+                pass
+            else:
+                vars[name] = value
 
     for name in cvars.unbound:
         if name in exclude_vars:
             continue
         try:
-            attr = _py_func(getattr(obj_, name))
+            value = _py_func(getattr(obj, name))
         except:
             continue
-            #raise TypeError(f'Attribute {name} is too complicated')
-        if hasattr(attr, '__call__') or inspect.ismodule(attr):
-            deps += extract_deps_code(attr, name=name)
+        if inspect.ismodule(value):
+            modules[name] = value
+        elif inspect.isbuiltin(value) or inspect.isfunction(value):
+            funcs[name] = value
+            r = re.search(rf'(\w+).{name}', code)
+            if r is not None and r.group() != 'self':
+                funcs[f'{r.group()}.{name}'] = value
+        elif inspect.isclass(value):
+            pass
         else:
-            text += f'    {name} = {repr(attr)}\n'
+            vars[name] = value
 
-    text += ''.join(textwrap.dedent(code).split(':\n')[1:])
-    text = '\n' + text.replace('self.', '').replace('array', 'np.array')
+    return vars, funcs, modules
 
-    return deps, text
 
-def extract_deps_code(obj, name=''):
-    """
-    Here obj can be:
-    - module that should be imported
-    - function which dependencies should be extracted (recursion)
-    - builtin which code can't be extracted therefore import this builtin
-    """
-    o = _py_func(obj)
+def extract_code(obj, exclude_vars=tuple(), method='__call__', **jitkwargs):
+    obj = _py_func(obj)
 
+    if inspect.isfunction(obj) or inspect.isbuiltin(obj):
+        code = inspect.getsource(obj)
+        vars, funcs, modules = unpack_cvars(obj, exclude_vars=exclude_vars)
+    else:
+        try:
+            attr = getattr(obj, method)
+        except:
+            return '', ''
+        code = inspect.getsource(attr)
+        vars, funcs, modules = unpack_cvars(obj, attr, exclude_vars=exclude_vars)
+
+    deps = ''
     text = ''
-    if inspect.ismodule(o):
-        return f'import {o.__name__}' + f' as {name}' if name else ''
-    elif inspect.isbuiltin(o):
-        return f'from {o.__module__} import {o.__name__}'
-    elif inspect.isfunction(o):
-        cvars = extract_closure_vars(o)
-        tmp = ''
-        for v, g in cvars.globals.items():
-            tmp += extract_deps_code(g, name=v) + '\n'
-            #text += extract_function_code(g).replace('@cfunc', '@jit').replace(g.__name__, v) + '\n'
-        if tmp:
-            text += extract_function_code(o).replace('@cfunc', '@jit').replace(o.__name__, name) + '\n'
+    classnames = []
+    for name, value in vars.items():
+        if inspect.isclass(value):
+            classnames.append(name)
+            continue
+        text += f'{name} = {repr(value)}\n'
+    for name, m in modules.items():
+        deps += extract_deps(m, name, **jitkwargs)
+    for name, f in funcs.items():
+        deps += extract_deps(f, name, **jitkwargs)
 
-    return text
+    code = textwrap.dedent(code.split('):\n')[1])
+    source = (text + code).replace('self.','').replace('array', 'np.array')
+    #for name in classnames:
+    #    source = source.replace(f"{name}.", '')
+
+    return deps, source
+
+
+def extract_deps(obj, name_='', **jitkwargs):
+    obj = _py_func(obj)
+
+    if inspect.ismodule(obj):
+        return f'import {obj.__name__}' + f' as {name_}\n' if name_ else '\n'
+    elif inspect.isbuiltin(obj) or inspect.isfunction(obj):
+        try:
+            code = inspect.getsource(obj)
+        except:
+            try:
+                module = importlib.import_module(obj.__module__)
+                obj = getattr(module, obj.__name__)
+                code = inspect.getsource(obj)
+            except:
+                return f'from {obj.__module__} import {obj.__name__}' + f' as {name_}\n' if name_ else '\n'
+        vars, funcs, modules = unpack_cvars(obj)
+        deps = ''
+        text = ''
+        #classnames = []
+        for name, value in vars.items():
+            if inspect.isclass(value):
+                #classnames.append(name)
+                continue
+            text += f'{name} = {repr(value)}\n'
+        for name, m in modules.items():
+            deps += extract_deps(m, name)
+        for name, f in funcs.items():
+            deps += extract_deps(f, name)
+
+        code += f'\n{name_} = {obj.__name__}\n' if name_ else ''
+        spl = code.split('):\n')
+        source = spl[0] + '):\n' + text + spl[1]
+
+        if source.startswith('@cfunc') or source.startswith('@jit') or source.startswith('@njit'):
+            source = source.replace('@cfunc', '@jit')
+        else:
+            source = f'@jit({dict2str(jitkwargs)})\n' + source
+
+        return deps + '\n' + source
+
+
+    raise NotImplemented(f'for objects like {obj}')
 
 class EventsCodeGen:
     def __init__(self,
@@ -160,7 +194,7 @@ class EventsCodeGen:
 
     def _extract_events_code(self, exclude_vars):
         for ev in self.events:
-            deps, code = extract_code(ev, exclude_vars, self.method)
+            deps, code = extract_code(ev, exclude_vars, self.method, **self.jitkwargs)
             self.ev_code.append(code)
             self.deps_code.append(deps)
 
@@ -169,13 +203,13 @@ class EventsCodeGen:
 
 
     def _generate_events_code(self):
-        text = 'from numba import jit, cfunc\nimport numpy as np\n\n'
+        text = 'from numba import jit, njit, cfunc\nimport numpy as np\n\n'
         jkw = dict2str(self.jitkwargs)
 
         for i, evtxt in enumerate(self.ev_code):
             text += self.deps_code[i] + '\n'
             text += f'@jit({jkw})\ndef event_{i}(t, s, value):\n'
-            text += evtxt + '\n'
+            text += textwrap.indent(evtxt, '    ') + '\n'
 
         text += generate_call_event(len(self.events), **self.jitkwargs)
         text += '\n'
